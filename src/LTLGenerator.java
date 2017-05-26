3a1ef22795b1603a9443b3d2db6b1e2e840b426d
Amabel <luoweibinb@gmail.com>
2017-05-26 17:41:32 +0900

    Add dom4j

---
+++ src/LTLGenerator.java
@@ -0,0 +1,19 @@
+/**
+ * @author  Weibin Luo
+ * @version Created on 2017/05/26 17:35:26
+ */
+public class LTLGenerator {
+	
+	private String fileName;
+	
+	public LTLGenerator(String fileName) {
+		this.fileName = fileName;
+	}
+	
+	public String generate() {
+		
+		
+		
+		return null;
+	}
+}
